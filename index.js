47ab08038558a442c27625592b875b44fbcb907f
mtheggi <mtheggi@gmail.com>
2023-10-09 00:22:49 +0300

    add logic in js  + css imgs bg

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
 
 
-function btnclick() {
-    var aud = new Audio("./sounds/crash.mp3");
-    aud.play(); 
+function playMusic(path, btntxt) {
+    var aud = new Audio(path);
+    aud.play();  
+    btntxt= btntxt.toUpperCase(); 
+    element= document.querySelector("."+btntxt);
+    var originalBoxShadow = element.style.boxShadow;
+    element.style.boxShadow = "0 8px 16px 0 red, 0 6px 20px 0 red";
+    setTimeout(() => {
+        element.style.boxShadow = originalBoxShadow;
+    }, 100);
 }
-var btns = document.querySelectorAll(".drum"); 
-for(let i =0 ;i < btns.length ;  i++) {
-    btns[i].addEventListener("click" , btnclick); 
-}
+
+var path = "./sounds/tom-1.mp3"; 
+document.addEventListener("keydown" ,function(event) {
+    //  console.log(event);
+    var btntxt = event.key;
+
+    switch (btntxt) {
+        case "w":
+            path = "./sounds/tom-1.mp3";    
+            playMusic(path,btntxt);
+        break;
+        case "a":
+            path = "./sounds/tom-2.mp3";    
+            playMusic(path,btntxt);
+        break;
+        case "s":
+            path = "./sounds/tom-3.mp3";    
+            playMusic(path,btntxt);
+        break;
+        case "d":
+            path = "./sounds/tom-4.mp3";    
+            playMusic(path,btntxt);
+        break;
+        case "l":
+            path = "./sounds/snare.mp3";    
+            playMusic(path,btntxt);
+        break;
+        case "k":
+            path = "./sounds/kick-bass.mp3";    
+            playMusic(path,btntxt);
+        break;
+        case "j":
+            path = "./sounds/crash.mp3";    
+            playMusic(path,btntxt);
+        break;
+        default:
+            break;
+    }
+}); 
 
 
